33be0eed20b055863dfa57e26b61ca514f2c03f9
Ando Roots <ando@sqroot.eu>
2012-12-31 20:34:41 +0200

    Add sorting to JSON response

--- classes/Kohana/HAPI/Response/JSON.php
+++ classes/Kohana/HAPI/Response/JSON.php
@@ -9,9 +9,28 @@
 class Kohana_HAPI_Response_JSON extends Kohana_HAPI_Response_Encoder implements HAPI_Response_Encodable
 {
 
+	/**
+	 * Implement response data sorting function.
+	 * Override and return the data unchanged to disable
+	 *
+	 * @param $response_data
+	 * @return bool
+	 */
+	public function sort_response($response_data)
+	{
+		asort($response_data);
+		return $response_data;
+	}
+
+	/**
+	 * Encode the response data
+	 *
+	 * @since 1.0
+	 * @return string Encoded data
+	 */
 	public function encode()
 	{
-		return json_encode($this->_data);
+		return json_encode($this->sort_response($this->_data));
 	}
 
 
