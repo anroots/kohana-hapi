dbab62d22a8b122c40f93969da56705f822ca1f5
Ando Roots <ando@sqroot.eu>
2013-02-11 13:24:09 +0200

    Add HAPI_Model abstract class and trait to transform ORM into HAPI responses

---
+++ classes/HAPI/Model.php
@@ -0,0 +1,5 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+abstract class HAPI_Model extends Kohana_HAPI_Model
+{
+
+}
---
+++ classes/Kohana/HAPI/Model.php
@@ -0,0 +1,31 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+abstract class Kohana_HAPI_Model
+{
+
+	protected $_orm;
+
+	public function __construct(ORM $orm)
+	{
+		$this->_orm = $orm;
+	}
+
+
+	/**
+	 * @param ORM $orm
+	 * @return HAPI_Model
+	 */
+	public static function factory(ORM $orm)
+	{
+		$class =get_class($orm);
+		$class = preg_replace('/Model/', 'HAPI_Model', $class, 1);
+		return new $class($orm);
+	}
+
+	/**
+	 * @return array
+	 */
+	public function transform()
+	{
+		return $this->_orm->as_array();
+	}
+}
