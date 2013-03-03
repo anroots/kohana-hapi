bbe7d3530773c08690bd3fb3256412805b8508f1
Ando Roots <ando@sqroot.eu>
2013-03-04 01:02:41 +0200

    Exceptions are of type application-problem+json

--- classes/HAPI/HAPI/HTTP/Exception.php
+++ classes/HAPI/HAPI/HTTP/Exception.php
@@ -1,6 +1,6 @@
 <?php defined('SYSPATH') or die('No direct script access.');
-class HAPI_HAPI_HTTP_Exception extends Kohana_HTTP_Exception {
-
+class HAPI_HAPI_HTTP_Exception extends Kohana_HTTP_Exception
+{
 
 	/**
 	 * Generate a Response for the current Exception
@@ -22,15 +22,14 @@ class HAPI_HAPI_HTTP_Exception extends Kohana_HTTP_Exception {
 	 */
 	public static function response(Exception $e)
 	{
+		// Log the exception
+		Kohana_Exception::log($e);
+
 		try
 		{
 			// Get the exception information
-			$class = get_class($e);
 			$code = $e->getCode();
 			$message = $e->getMessage();
-			$file = $e->getFile();
-			$line = $e->getLine();
-			$trace = $e->getTrace();
 
 			if (! headers_sent())
 			{
@@ -38,64 +37,12 @@ class HAPI_HAPI_HTTP_Exception extends Kohana_HTTP_Exception {
 				$http_header_status = ($e instanceof HTTP_Exception) ? $code : 500;
 			}
 
-			/**
-			 * HTTP_Exceptions are constructed in the HTTP_Exception::factory()
-			 * method. We need to remove that entry from the trace and overwrite
-			 * the variables from above.
-			 */
-			if ($e instanceof HTTP_Exception AND $trace[0]['function'] == 'factory')
+
+			if ($e instanceof ErrorException && isset(Kohana_Exception::$php_errors[$code]))
 			{
-				extract(array_shift($trace));
+				// Use the human-readable error name
+				$code = Kohana_Exception::$php_errors[$code];
 			}
-
-
-			if ($e instanceof ErrorException)
-			{
-				/**
-				 * If XDebug is installed, and this is a fatal error,
-				 * use XDebug to generate the stack trace
-				 */
-				if (function_exists('xdebug_get_function_stack') AND $code == E_ERROR)
-				{
-					$trace = array_slice(array_reverse(xdebug_get_function_stack()), 4);
-
-					foreach ($trace as & $frame)
-					{
-						/**
-						 * XDebug pre 2.1.1 doesn't currently set the call type key
-						 * http://bugs.xdebug.org/view.php?id=695
-						 */
-						if (! isset($frame['type']))
-						{
-							$frame['type'] = '??';
-						}
-
-						// XDebug also has a different name for the parameters array
-						if (isset($frame['params']) AND ! isset($frame['args']))
-						{
-							$frame['args'] = $frame['params'];
-						}
-					}
-				}
-
-				if (isset(Kohana_Exception::$php_errors[$code]))
-				{
-					// Use the human-readable error name
-					$code = Kohana_Exception::$php_errors[$code];
-				}
-			}
-
-			/**
-			 * The stack trace becomes unmanageable inside PHPUnit.
-			 *
-			 * The error view ends up several GB in size, taking
-			 * serveral minutes to render.
-			 */
-			if (defined('PHPUnit_MAIN_METHOD'))
-			{
-				$trace = array_slice($trace, 0, 2);
-			}
-
 
 			// Prepare the response object.
 			$response = Response::factory();
@@ -104,23 +51,13 @@ class HAPI_HAPI_HTTP_Exception extends Kohana_HTTP_Exception {
 			$response->status(($e instanceof HTTP_Exception) ? $e->getCode() : 500);
 
 			// Set the response headers
-			$response->headers('Content-Type', 'application/json; charset='.Kohana::$charset);
+			$response->headers('Content-Type', 'application/api-problem+json; charset='.Kohana::$charset);
 
 			$response_data = [
-				'code'    => $code,
-				'message' => $message
+				'describedBy' => 'http://en.wikipedia.org/wiki/List_of_HTTP_status_codes', // Todo
+				'httpStatus'  => $code,
+				'title'       => $message
 			];
-
-			if (Kohana::$environment !== Kohana::PRODUCTION)
-			{
-				$additional_data = [
-					'class' => $class,
-					'file'  => $file,
-					'line'  => $line,
-					'trace' => $trace
-				];
-				$response_data = array_replace($response_data, $additional_data);
-			}
 
 			// Set the response body
 			$response->body(json_encode($response_data));
